492ae0bbb3b6972528e3976fd567a803e2a827f8
SzentkMarton <marton.szentkiralyi@stud.uni-corvinus.hu>
2022-10-13 12:19:32 +0200

    feladat

--- Sudoku/Form1.Designer.cs
+++ Sudoku/Form1.Designer.cs
@@ -29,13 +29,32 @@ namespace Sudoku
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Location = new System.Drawing.Point(112, 48);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(280, 280);
+            this.panel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- Sudoku/Form1.cs
+++ Sudoku/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,67 @@ namespace Sudoku
 {
     public partial class Form1 : Form
     {
+        private Random rng = new Random();
+        private Sudoku _current = new Sudoku();
+
+        List<Sudoku> _sudokus = new List<Sudoku>();
         public Form1()
         {
             InitializeComponent();
+            CreatePlayField();
+            LoadSudokus();
+            
+            NewGame();
+        }
+        private void CreatePlayField()
+        {
+            int linewidth = 5;
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    SudokuFields sf = new SudokuFields();
+                    sf.Left = j * sf.Width + (int)(Math.Floor((double)(j / 3))) * linewidth;
+                    sf.Top = i * sf.Height + (int)(Math.Floor((double)(i / 3))) * linewidth;
+                    panel1.Controls.Add(sf);
+                }
+            }
+        }
+        private void LoadSudokus()
+        {
+            _sudokus.Clear();
+            using (StreamReader sr = new StreamReader("sudoku.csv", Encoding.Default))
+            {
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(',');
+                    Sudoku s = new Sudoku();
+                    s.Quiz = line[0];
+                    s.Solution = line[1];
+                    _sudokus.Add(s);
+                }
+
+            }
+        }
+        private Sudoku GetRandomQuiz()
+        {
+            int randomNumber = rng.Next(_sudokus.Count);
+            return _sudokus[randomNumber];
+
+
+        }
+        private void NewGame()
+        {
+            _current = GetRandomQuiz();
+            int counter = 0;
+
+            foreach (var mezok in panel1.Controls.OfType<SudokuFields>())
+            {
+                mezok.Value = int.Parse(_current.Quiz[counter].ToString());
+
+            }
+
         }
     }
 }
