95ccaaea495e490b94826261192c5958841723aa
SzentkMarton <marton.szentkiralyi@stud.uni-corvinus.hu>
2022-10-21 12:48:38 +0200

    task6

--- MNBCurrencyReader/MNBCurrencyReader/Form1.cs
+++ MNBCurrencyReader/MNBCurrencyReader/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace MNBCurrencyReader
@@ -25,13 +26,36 @@ namespace MNBCurrencyReader
         public Form1()
         {
             InitializeComponent();
-            Arfolyamok();
+            LoadXML();
+
 
             
 
 
             dataGridView1.DataSource = rates;
+            Diagram();
+            
 
+
+        }
+
+        public void Diagram()
+        {
+            chartRateData.DataSource = rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
         public void LoadXML()
         {
